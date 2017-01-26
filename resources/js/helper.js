22bff41b35c497fc1dd527dd358d3a571a66e24e
Tiago Maia <tiago.maia.lopes@gmail.com>
2017-01-26 11:04:47 -0200

    Adding polymer properties to the about and related components.

--- resources/js/helper.js
+++ resources/js/helper.js
@@ -1,25 +1,48 @@
 // Jquery's document ready handler
 $(function() {
 
-  // Replaces the %data% occurrences on the text 
+  // Replaces the %data% occurrences on the text
   // passed with the data argument provided.
   var replaceData = function(text, data) {
-    return text.replace("%data%", "[" + JSON.stringify(data) + "]");
+    return text.replace("%data%", JSON.stringify(data));
   }
 
-  // Object tree responsible for keeping the resume 
+  // Object tree responsible for keeping the resume
   // information used in the template.
   var author = {
     bio: {
-      name: "",
-      role: "",
-      welcomeMessage: "",
+      name: "Tiago Maia",
+      role: "Mobile / Web Developer",
+      welcomeMessage: "Lorem ipsum dolor sit amet, consectetur \
+      adipiscing elit, sed do eiusmod tempor incididunt \
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam,\
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea \
+      commodo consequat.",
+      about: "Duis aute irure dolor in reprehenderit in voluptate \
+      velit esse cillum dolore eu fugiat nulla pariatur. \
+      Excepteur sint occaecat cupidatat non proident, \
+      sunt in culpa qui officia deserunt mollit anim id est laborum.",
       contacts: {
-
+        github: '#',
+        email: '#',
+        linkedin: '#'
       },
-      skills: [
-
-      ],
+      skills: 'js, html, css, php, objective-c',
+      devicons: {
+        html: 'devicon-html5-plain',
+        css: 'devicon-css3-plain',
+        sass: 'devicon-sass-original',
+        jquery: 'devicon-javascript-plain',
+        javascript: 'devicon-jquery-plain',
+        node: 'devicon-nodejs-plain',
+        bower: 'devicon-bower-plain',
+        gulp: 'devicon-gulp-plain',
+        php: 'devicon-php-plain',
+        laravel: 'devicon-laravel-plain',
+        git: 'devicon-git-plain',
+        github: 'devicon-github-plain',
+        ios: 'devicon-apple-original'
+      },
       pictureUrl: "",
       display: function() {
         $('section.about').append(
@@ -29,9 +52,9 @@ $(function() {
     },
 
     work: {
-      jobs: [ 
+      jobs: [
         {
-          employer: "", 
+          employer: "",
           title: "",
           location: "",
           dates: "",
